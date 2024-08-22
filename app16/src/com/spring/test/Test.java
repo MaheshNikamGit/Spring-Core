dff1244332858e477248ce694601108f4973cf33
Mahesh Nikam <48942366+MaheshNikamGit@users.noreply.github.com>
2024-08-22 23:16:06 +0530

    setter() method dependancy injection example - 1

---
+++ app16/src/com/spring/test/Test.java
@@ -0,0 +1,16 @@
+package com.spring.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.beans.Employee;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new ClassPathXmlApplicationContext("/com/spring/resources/applicationContext.xml");
+		Employee employee = (Employee)context.getBean("empBean");
+		employee.getEmployeeDetails();
+	}
+
+}
