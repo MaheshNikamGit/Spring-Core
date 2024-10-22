1f51c4ae0058e06c00bede30b6259ed0793333ec
Mahesh Nikam <48942366+MaheshNikamGit@users.noreply.github.com>
2024-10-22 21:17:48 +0530

    Autowire + Explicit Wiring

---
+++ app26/src/com/spring/beans/Course.java
@@ -0,0 +1,27 @@
+package com.spring.beans;
+
+public class Course {
+	public String cid;
+	public String cname;
+	public float ccost;
+	public String getCid() {
+		return cid;
+	}
+	public void setCid(String cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public float getCcost() {
+		return ccost;
+	}
+	public void setCcost(float ccost) {
+		this.ccost = ccost;
+	}
+	
+	
+}
